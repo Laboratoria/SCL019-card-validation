a04e0ef5a2c332470203054b64ab3f9beb29385e
DanielaAlcalaDaboin <daniela.alcalad@gmail.com>
2021-12-02 23:29:39 -0300

    css: Diseño basico de una tarjeta, html: esta la estructira basica dentro de section y en js: tenemos el array que da el numero de la tdc invertido

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-import validator from './validator.js';
 
-console.log(validator);
+//import validator from './validator.js';
+
+//console.log(validator);
+let cardNumber;
+let arrayNumeros= new Array(16);
+
+ 
+
+
+
+function getData() {
+
+    let cardNumber = document.getElementById("cardNumber").value;
+    // let cardName = document.getElementById("carddHolderName").value;
+    //let date = document.getElementById("expiryDate").value;
+    //let security = document.getElementById("securityNumber").value;
+    //alert("Hola"+ cardNumber);
+   //Se crea el arreglo 
+  // for (let i=0; i < cardNumber.length; i++)
+  let x=0; 
+  for (let i=cardNumber.length - 1 ; i >= 0; i--){
+        arrayNumeros[x] = cardNumber[i];  
+        x=x+1;
+    }
+    console.log(arrayNumeros);
+    // Se hace un orden inverso del array
+    //arrayNumeros.reverse();*/
+}   
+document.getElementById ("pagar").onclick = getData;
