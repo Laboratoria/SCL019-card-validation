1589c9b844f2360ea728bdf99aa5b45f70d0bde8
JavieraMunita <95220695+JavieraMunita@users.noreply.github.com>
2021-12-14 10:18:31 -0300

    Primera actualización

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import validator from './validator.js';
 
-console.log(validator);
+const submitCardNumber= document.getElementById("numSubmit");
+
+submitCardNumber.addEventListener("click", () => {
+  const numeroTarjeta= document.getElementById("cardNumber").value;
+
+  let maskNumber = validator.maskify(numeroTarjeta);
+
+  
+  if (numeroTarjeta.length <= 12 || numeroTarjeta.length >= 19){
+    alert("Debe ingresar un número que tenga entre 13 y 18 dígitos");
+  } else { 
+    if (validator.validarTarjeta(numeroTarjeta) ) {
+      alert("Tu tarjeta " + maskNumber + " es válida");
+    } else {
+      alert("Tu tarjeta " + maskNumber + " es inválida");
+   }
+
+  }
+
+  
+
+});
+
+console.log(validator); 
+
+
+
