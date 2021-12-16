869ccba0b69eb87f90bf58ba79ac964b24e5ce6b
May Ortega Torres <mayorlen.ortega@gmail.com>
2021-12-16 19:33:05 -0300

    Se agregó nombre

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ const submitCardNumber= document.getElementById("numSubmit");
 submitCardNumber.addEventListener("click", () => {
   const numeroTarjeta= document.getElementById("cardNumber").value;
 
+  let nombre = document.getElementById("nom").value;
   let maskNumber = validator.maskify(numeroTarjeta);
 
-  
-  if (numeroTarjeta.length <= 12 || numeroTarjeta.length >= 19){
-    alert("Debe ingresar un número que tenga entre 13 y 18 dígitos");
-  } else { 
-    if (validator.isValid(numeroTarjeta) ) {
-      alert("Tu tarjeta " + maskNumber + " es válida. ¡Estamos listos para el despegue!");
+  if (numeroTarjeta.length <= 12 || numeroTarjeta.length >= 19) {
+    alert(nombre + " :Debes ingresar un número que tenga entre 13 y 18 dígitos");
+  } else {
+    if (validator.validarTarjeta(numeroTarjeta)) {
+      alert(nombre + " :Tu tarjeta " + maskNumber + " es válida. ¡Estamos listos para el despegue!");
     } else {
       alert("¡Houston, tenemos un problema! Tu tarjeta " + maskNumber + " es inválida");
    }
