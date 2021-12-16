55d303efbbe4c5291566a1aabdf4a334446a9a7b
JavieraMunita <95220695+JavieraMunita@users.noreply.github.com>
2021-12-16 10:22:56 -0300

    git commit "Tercera actualización"

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ submitCardNumber.addEventListener("click", () => {
     alert("Debe ingresar un número que tenga entre 13 y 18 dígitos");
   } else { 
     if (validator.validarTarjeta(numeroTarjeta) ) {
-      alert("Tu tarjeta " + maskNumber + " es válida");
+      alert("Tu tarjeta " + maskNumber + " es válida. ¡Estamos listos para el despegue!");
     } else {
-      alert("Tu tarjeta " + maskNumber + " es inválida");
+      alert("¡Houston, tenemos un problema! Tu tarjeta " + maskNumber + " es inválida");
    }
 
   }
