a3cb740e59d071e019cdfb7168fb4f832e5891ea
May_Ortega <91227867+MayorlenO@users.noreply.github.com>
2021-12-17 19:55:10 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,10 +11,10 @@ submitCardNumber.addEventListener("click", () => {
 
 
   if (numeroTarjeta.length <= 12 || numeroTarjeta.length >= 19) {
-    alert(nombre + " :Debes ingresar un número que tenga entre 13 y 18 dígitos");
+    alert(nombre + ": Debes ingresar un número que tenga entre 13 y 18 dígitos");
   } else {
     if (validator.isValid(numeroTarjeta)) {
-      alert(nombre + " :Tu tarjeta " + maskNumber + " es válida. ¡Estamos listos para el despegue!");
+      alert(nombre + ": Tu tarjeta " + maskNumber + " es válida. ¡Estamos listos para el despegue!");
     } else {
       alert("¡Houston, tenemos un problema! Tu tarjeta " + maskNumber + " es inválida");
    }
